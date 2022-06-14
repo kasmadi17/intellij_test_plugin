53ceae4d017f0afec2f48b25c7588db549c17620
GitHub Action <action@github.com>
2022-06-14 05:48:47 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/kasmadi17/intellijtestplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.kasmadi17.intellijtestplugin.services
+
+import com.github.kasmadi17.intellijtestplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
